241875103b0fd100a80452d623dcb7af400b33b5
Edgar Piera <edgar.piera@neogrup.com>
2021-05-04 17:03:08 +0200

    - Integrate with virtual keyboard

--- nc-password-dialog.js
+++ nc-password-dialog.js
@@ -8,6 +8,7 @@ import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/communication-icons.js';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/iron-a11y-keys/iron-a11y-keys.js';
+import '@neogrup/nc-keyboard/nc-keyboard.js';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
 import { timeOut } from '@polymer/polymer/lib/utils/async.js';
 import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
@@ -18,9 +19,30 @@ class NcPasswordDialog extends mixinBehaviors([AppLocalizeBehavior], PolymerElem
   static get template() {
     return html`
       <style>
+        paper-dialog.modalNoApp {
+          width: 95%;
+        }
+
+        @media (min-width: 1660px) {
+          paper-dialog {
+            max-width: 1460px;
+          }
+        }
+
+        .content{
+          margin-top: 0px;
+        }
+
+        paper-button.delete:not([disabled]){
+          background-color: var(--error-color);
+        }
+
+        paper-button.accept:not([disabled]){
+          background-color: var(--success-color);
+        }
       </style>
       
-      <paper-dialog id="passwordDialog" class="modalNoApp" modal dialog>
+      <paper-dialog id="passwordDialog" class="modalNoApp fullWidth" modal dialog>
         <iron-a11y-keys id="a11ySignIn" keys="enter" on-keys-pressed="_accept"></iron-a11y-keys>
         <div class="header">
           <iron-icon icon="communication:vpn-key"></iron-icon><h3>{{localize(dialogTitle)}}</h3>
@@ -28,12 +50,21 @@ class NcPasswordDialog extends mixinBehaviors([AppLocalizeBehavior], PolymerElem
         <div class="content">
           <paper-input id="password"  type="password" error-message="{{localize('INPUT_ERROR_REQUIRED')}}" value="{{formData.password}}" on-focus="_setFocus" on-blur="_setBlur" required></paper-input>
         </div>
+        <div class="content-keyboard">
+          <nc-keyboard
+            keyboard-enabled="{{showKeyboard}}"
+            keyboard-embedded='S'
+            keyboard-type="keyboard"
+            value="{{formData.password}}"
+            >
+          </nc-keyboard>
+        </div>
         <div class="buttons">
           <div hidden\$="{{!loading}}">
             <paper-spinner class="dialog" active></paper-spinner>
           </div>
-          <paper-button raised dialog-dismiss disabled\$="[[loading]]">{{localize('BUTTON_CLOSE')}}</paper-button>
-          <paper-button raised disabled\$="[[loading]]" on-tap="_accept">{{localize('BUTTON_ACCEPT')}}</paper-button>
+          <paper-button raised class="delete" dialog-dismiss disabled\$="[[loading]]">{{localize('BUTTON_CLOSE')}}</paper-button>
+          <paper-button raised class="accept" disabled\$="[[loading]]" on-tap="_accept">{{localize('BUTTON_ACCEPT')}}</paper-button>
         </div>
       </paper-dialog>
     `;
@@ -60,6 +91,9 @@ class NcPasswordDialog extends mixinBehaviors([AppLocalizeBehavior], PolymerElem
         value: ''
       },
       dialogOrigin: String,
+      showKeyboard: {
+        type: String,
+      },
     };
   }
 
